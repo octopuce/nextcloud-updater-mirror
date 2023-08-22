6e12c2c098fef197d50567422e6821fd60bc0a6f
Benjamin Sonntag <benjamin@octopuce.fr>
2023-08-22 11:46:49 +0200

    [fix] rsync was missing a -a and added force parameters to update.sh for the first update...

--- update.sh
+++ update.sh
@@ -3,10 +3,10 @@
 # update the official updater :
 cd /var/www/nextcloud/updater_server.orig
 NEW=$(LC_ALL=C git pull | wc -l)
-if [ $NEW -gt 1 ]
+if [ $NEW -gt 1 -o "$1" = "force" ]
 then
     # something was updated on the original source, let's patch ! 
-    rsync /var/www/nextcloud/updater_server.orig/ /var/www/nextcloud/updater_server/ --delete --exclude "vendor/" 
+    rsync -a /var/www/nextcloud/updater_server.orig/ /var/www/nextcloud/updater_server/ --delete --exclude "vendor/" 
     cd /var/www/nextcloud/updater_server/
     patch <../index.diff
     cat config/config.php |sed -e 's#download.nextcloud.com#ncdownload.octopuce.fr#g' >../octopuce-config.php
